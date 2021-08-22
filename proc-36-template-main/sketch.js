6f038b439be9ebaf9d1d2648c4fbfd6206f257b2
harithaharidas9061 <48945616+harithaharidas9061@users.noreply.github.com>
2021-08-22 20:15:26 +0530

    Update sketch.js

--- proc-36-template-main/sketch.js
+++ proc-36-template-main/sketch.js
@@ -1,11 +1,8 @@
 var dog,sadDog,happyDog, database;
 var foodS,foodStock;
-var addFood;
-var foodObj; 
-var feed,lastFed
-
-//create feed and lastFed variable here
-
+var fedTime,lastFed;
+var feed,addFood;
+var foodObj;
 
 function preload(){
 sadDog=loadImage("Dog.png");
@@ -17,47 +14,42 @@ function setup() {
   createCanvas(1000,400);
 
   foodObj = new Food();
- 
+
   foodStock=database.ref('Food');
   foodStock.on("value",readStock);
   
   dog=createSprite(800,200,150,150);
   dog.addImage(sadDog);
   dog.scale=0.15;
-
-  //create feed the dog button here
+  
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
 
   addFood=createButton("Add Food");
   addFood.position(800,95);
   addFood.mousePressed(addFoods);
 
-  feed=createButton("Feed the Dog")
-  feed.position(700,95)
-  feed.mousePressed(feedDog)
 }
 
 function draw() {
   background(46,139,87);
   foodObj.display();
 
-  //write code to read fedtime value from the database 
-  
-   fedTime=database.ref("FeedTime")
-   fedTime.on("value",function(data){
-   lastFed=data.val()
-   })
-  //write code to display text lastFed time here
-
-  fill(255,255,255)
-
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  fill(255,255,254);
+  textSize(15);
   if(lastFed>=12){
-    text("Last Food:"+ lastFed%+"PM",350,30)
-  }else if(lastFed==0){
-    text("Last Feed: 12 AM",350,30)
-  }else{
-    text("Last Feed:"+lastFed+"AM",350,30)
-  }
-
+    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 350,30);
+   }
  
   drawSprites();
 }
@@ -69,24 +61,23 @@ function readStock(data){
 }
 
 
+//function to update food stock and last fed time
 function feedDog(){
   dog.addImage(happyDog);
-
-  //write code here to update food stock and last fed time
+  
   var food_stock_val = foodObj.getFoodStock();
   if(food_stock_val <= 0){
-    foodObj.updateFoodStock(food_stock_val * 0);
+      foodObj.updateFoodStock(food_stock_val *0);
   }else{
-    foodObj.updateFoodStock(food_stock_val -1)
-
+      foodObj.updateFoodStock(food_stock_val -1);
   }
+  
   database.ref('/').update({
     Food:foodObj.getFoodStock(),
     FeedTime:hour()
   })
-
 }
- 
+
 //function to add food in stock
 function addFoods(){
   foodS++;
